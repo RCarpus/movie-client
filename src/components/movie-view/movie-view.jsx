f38194c1143b636c741222f21511caf5dbe409da
RCarpus <40838987+RCarpus@users.noreply.github.com>
2021-11-27 22:25:10 -0500

    updated styles

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -14,26 +14,40 @@ export class MovieView extends React.Component {
     const { movie, onBackClick } = this.props;
     return (
       <div className='movie-view'>
-        <div className="movie-poster">
-          <img src={imgPath + movie.ImagePath}  />
+        
+        {/* This is on top of grid */}
+        <div className="movie-view__title-line">
+          <button className="movie-view-button" onClick={() => { onBackClick(null); }}>&lt;</button>
+          <span className="movie-view__title">{movie.Title}</span>
+          <button className="movie-view-button" >&#10032;</button>
         </div>
-        <div className="movie-view__line">
-          <span className="movie-view__line__label">Title: </span>
-          <span className="movie-view__line__value">{movie.Title}</span>
+
+        <div className='movie-view__grid'>
+
+          {/* Grid column 1 */}
+          <div className='movie-info'>
+            <div className="movie-view__line">
+              <span className="movie-view__line__label">Genre: </span>
+              <span className="movie-view__line__value">{movie.Genre.Name}</span>
+            </div>
+
+            <div className="movie-view__line">
+              <span className="movie-view__line__label">Director: </span>
+              <span className="movie-view__line__value">{movie.Director.Name}</span>
+            </div>
+
+            <div className="movie-view__line description">
+              <span className="movie-view__line__label">Description: </span>
+              <span className="movie-view__line__value">{movie.Description}</span>
+            </div>
+          </div>
+
+          {/* Grid column 2 */}
+          <div className="movie-poster">
+            <img src={imgPath + movie.ImagePath}  />
+          </div>
+
         </div>
-        <div className="movie-view__line">
-          <span className="movie-view__line__label">Genre: </span>
-          <span className="movie-view__line__value">{movie.Genre.Name}</span>
-        </div>
-        <div className="movie-view__line">
-          <span className="movie-view__line__label">Director: </span>
-          <span className="movie-view__line__value">{movie.Director.Name}</span>
-        </div>
-        <div className="movie-view__line">
-          <span className="movie-view__line__label">Description: </span>
-          <span className="movie-view__line__value">{movie.Description}</span>
-        </div>
-        <button onClick={() => { onBackClick(null); }}>Back</button>
       </div>
     );
   }
--- src/components/registration-view/registration-view.jsx
+++ src/components/registration-view/registration-view.jsx
@@ -1,52 +1,69 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
+import './registration-view.scss';
+
 export function RegistrationView(props) {
+  // Shorthands used with { useState } Reat Hook 
+  // https://reactjs.org/docs/hooks-state.html
   const [username, setUsername] = useState('');
   const [password1, setPassword1] = useState('');
   const [password2, setPassword2] = useState('');
   const [email, setEmail] = useState('');
   const [birthday, setBirthday] = useState('');
 
+  // Modify state of MainView to be registered and logged in with new user
   const handleSubmit = (e) => {
     e.preventDefault();
     props.onRegister(true, username);
   };
 
   return (
-    <div>
-      <p>Sign up for a free account:</p>
-      <form>
-        <label>
-          Username: 
-          <input type="text" value={username} onChange={e => setUsername(e.target.value)} />
-          <span className="label-tips">5+ characters, no spaces</span>
-          <br/>
-        </label>
-        <label>
-          Enter desired password: 
-          <input type="text" value={password1} onChange={e => setPassword1(e.target.value)} />
-          <span className="label-tips">must not be blank</span>
-          <br/>
-        </label>
-        <label>
-          Re-enter password: <span className="label-tips">passwords must match</span>
-          <input type="text" value={password2} onChange={e => setPassword2(e.target.value)} />
-          <span className="label-tips">passwords must match</span>
-          <br/>
-        </label>
-        <label>
-          Email: 
-          <input type="text" value={email} onChange={e => setEmail(e.target.value)} />
-          <span className="label-tips">required</span>
-          <br/>
-        </label>
-        <label>
-          Birthday: 
-          <input type="text" value={birthday} onChange={e => setBirthday(e.target.value)} />
-          <span className="label-tips">optional</span>
-          <br/>
-        </label>
+    <div class="registration-view">
+      <h2>Sign up for a free MyFlix account:</h2>
+
+      <form className="registration-form">
+
+        <div class="registration-form__line">
+          <label className="registration-form__line-label">
+            Username: 
+          </label>
+            <input class="registration-form__line__input-field" type="text" value={username} onChange={e => setUsername(e.target.value)} />
+            <span className="registration-form__label-tips">5+ characters, no spaces</span>
+        </div>
+        
+        <div class="registration-form__line">
+          <label className="registration-form__line-label">
+            Enter desired password: 
+          </label>
+            <input class="registration-form__line__input-field" type="text" value={password1} onChange={e => setPassword1(e.target.value)} />
+            <span className="registration-form__label-tips">must not be blank</span>
+        </div>
+        
+        <div class="registration-form__line">
+          <label className="registration-form__line-label">
+            Re-enter password:
+          </label>
+            <input class="registration-form__line__input-field" type="text" value={password2} onChange={e => setPassword2(e.target.value)} />
+            <span className="registration-form__label-tips">passwords must match</span>
+        </div>
+        
+        <div class="registration-form__line">
+          <label className="registration-form__line-label">
+            Email: 
+          </label>
+            <input class="registration-form__line__input-field" type="text" value={email} onChange={e => setEmail(e.target.value)} />
+            <span className="registration-form__label-tips">required</span>
+        </div>
+        
+        <div class="registration-form__line">
+          <label class="registration-form__line-label" className="registration-form__line">
+            Birthday: 
+          </label>
+            <input class="registration-form__line__input-field" type="text" value={birthday} onChange={e => setBirthday(e.target.value)} />
+            <span className="registration-form__label-tips">optional</span>
+        </div>
+        
 
         <button type="submit" onClick={handleSubmit}>Register</button>
       </form>
@@ -56,6 +73,6 @@ export function RegistrationView(props) {
 
 // prop-types
 // Give informational warnings in browser if data does not match required shape
-// LoginView.propTypes = {
-//   onLoggedIn: PropTypes.func.isRequired
-// };
+RegistrationView.propTypes = {
+  onRegister: PropTypes.func.isRequired
+};
