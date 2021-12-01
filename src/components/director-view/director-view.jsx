5751f4d4827d977c7c5d4284cdc950b7f1075152
RCarpus <40838987+RCarpus@users.noreply.github.com>
2021-12-01 09:19:07 -0500

    added MovieCards and updated layout

--- src/components/director-view/director-view.jsx
+++ src/components/director-view/director-view.jsx
@@ -3,35 +3,51 @@ import PropTypes from 'prop-types';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Button from 'react-bootstrap/Button';
+import { MovieCard } from '../movie-card/movie-card';
+import { Link } from "react-router-dom";
+
 
 import './director-view.scss';
-
 
 export class DirectorView extends React.Component {
 
   render() {
-    const { director, onBackClick } = this.props;
-    console.log(director);
+    const { director, onBackClick, movies } = this.props;
+    let moviesByDirector = movies.filter(m => m.Director.Name === director.Name).slice(0, 3);
     return (
-      <Row className='Director-view'>
-        <Col lg={8}>
-        <div className="Director-view__title-line">
+      <div className='director-view'>
+
+        <Row className="director-view__title-line">
+          <Col sm={12} md={6}>
             <Button id="back-button" onClick={() => { onBackClick(null); }}>&lt;</Button>
-            <span className="Director-view__title">{director.Name}</span>
+            <span className="director-view__title">{director.Name}</span>
+          </Col>
+          <Col>
+            <Link to={'/'}>
+              <Button className="btn btn-secondary btn-sm genre-view__title-line__nav" type="button">All movies</Button>
+            </Link>
+            <Link to={'/profile'}>
+              <Button className="btn btn-secondary btn-sm genre-view__title-line__nav" type="button">Profile</Button>
+            </Link>
+          </Col>
+
+        </Row>
+
+        <div className='director-view__info'>
+          <p className="director-view__info__description-label">Description</p>
+          <span className="director-view__info__description">{director.Bio}</span>
+          <p className="director-view__info__movie-list-label">Some movies from this director</p>
+
+          <Row>
+            {moviesByDirector.map(m => (
+              <Col md={4} key={m._id}>
+                <MovieCard movie={m} />
+              </Col>
+            ))}
+          </Row>
+
         </div>
-
-
-          <div className='Director-info'>
-            <div className="Director-view__line">
-              <span className="Director-view__line__label">Bio: </span>
-              <span className="Director-view__line__value">{director.Bio}</span>
-            </div>
-
-          </div>
-
-        </Col>
-
-      </Row>
+      </div>
     );
   }
 }
