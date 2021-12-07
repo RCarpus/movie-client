f69085b102d4d555225d5d8b15c9361ed06079e9
RCarpus <40838987+RCarpus@users.noreply.github.com>
2021-12-07 11:44:22 -0500

    updated ProfileView styles

--- src/components/profile-view/profile-view.jsx
+++ src/components/profile-view/profile-view.jsx
@@ -23,17 +23,17 @@ export class ProfileView extends React.Component {
   }
 
   updateUserData() {
-    if ( this.form.current.reportValidity() ) {
+    if (this.form.current.reportValidity()) {
       let Username = this.form.current[0].value;
       let Password = this.form.current[1].value;
       let Email = this.form.current[2].value;
       let Birthday = this.form.current[3].value;
       let updatedData = {};
-      if ( Username.length > 0 ) updatedData.Username = Username;
-      if ( Password.length > 0 ) updatedData.Password = Password;
-      if ( Email.length > 0 ) updatedData.Email = Email;
-      if ( Birthday.length > 0 ) updatedData.Birthday = Birthday;
-      
+      if (Username.length > 0) updatedData.Username = Username;
+      if (Password.length > 0) updatedData.Password = Password;
+      if (Email.length > 0) updatedData.Email = Email;
+      if (Birthday.length > 0) updatedData.Birthday = Birthday;
+
       let authHeader = { headers: { Authorization: `Bearer ${localStorage.getItem('token')}` } }
       Axios.put(`https://rcarpus-movie-api.herokuapp.com/users/${localStorage.getItem('user')}`, updatedData, authHeader)
         .then(response => {
@@ -53,12 +53,12 @@ export class ProfileView extends React.Component {
     Axios.delete(`https://rcarpus-movie-api.herokuapp.com/users/${this.props.userData.Username}/movies/${movie_id}`, {
       headers: { authorization: `Bearer ${localStorage.getItem('token')}` }
     })
-    .then(response => {
-      this.props.setUserData(response.data);
-    })
-    .catch(function (error) {
-      console.log(error);
-    });
+      .then(response => {
+        this.props.setUserData(response.data);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
   }
 
   unregister() {
@@ -67,14 +67,14 @@ export class ProfileView extends React.Component {
       Axios.delete(`https://rcarpus-movie-api.herokuapp.com/users/${this.props.userData.Username}`, {
         headers: { authorization: `Bearer ${localStorage.getItem('token')}` }
       })
-      .then(response => {
-        localStorage.clear();
-        this.props.setUserData({});
-        window.location.href = '/';
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
+        .then(response => {
+          localStorage.clear();
+          this.props.setUserData({});
+          window.location.href = '/';
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
     }
   }
 
@@ -84,103 +84,107 @@ export class ProfileView extends React.Component {
 
     return (
       <div className="profile-view">
-        <Row>
-          <Col sm={12} md={7}>
-            <h2>Profile</h2>
-          </Col>
-          <Col md={5}>
-            <Link to={'/'}>
-              <Button className="btn btn-secondary btn-sm genre-view__title-line__nav" type="button">All movies</Button>
-            </Link>
-          </Col>
-        </Row>
+        <div className="user-info">
+          <Row>
+            <Col sm={12} md={9}>
+              <h2>Profile</h2>
+            </Col>
+            <Col md={3}>
+              <Link to={'/'}>
+                <Button className="btn btn-secondary btn-sm genre-view__title-line__nav" type="button">All movies</Button>
+              </Link>
+            </Col>
+          </Row>
 
-        <Row>
-          <Col>
-            <Form className="update-info-form" ref={this.form} onSubmit={e => e.preventDefault()}>
+          <Row>
+            <Col xs={12} md={8} lg={6} xl={5}>
+              <Form className="update-info-form" ref={this.form} onSubmit={e => e.preventDefault()}>
 
-              <Form.Group>
+                <Form.Group>
+                  <Row>
+                    <Col xs={12} md={4}>
+                      <Form.Label className="user-info__form-label">
+                        Username
+                      </Form.Label>
+                    </Col>
+                    <Col>
+                      <Form.Control className="user-info__form-input" placeholder={this.props.userData.Username}
+                        pattern="^[a-zA-Z0-9]{5,}$" />
+                    </Col>
+                  </Row>
+                </Form.Group>
+
+                <Form.Group>
+                  <Row>
+                    <Col xs={12} md={4}>
+                      <Form.Label className="user-info__form-label">
+                        Password
+                      </Form.Label>
+                    </Col>
+                    <Col>
+                      <Form.Control className="user-info__form-input" placeholder={'new password'} />
+                    </Col>
+                  </Row>
+                </Form.Group>
+
+                <Form.Group>
+                  <Row>
+                    <Col xs={12} md={4}>
+                      <Form.Label className="user-info__form-label">
+                        E-mail
+                      </Form.Label>
+                    </Col>
+                    <Col>
+                      <Form.Control className="user-info__form-input" placeholder={this.props.userData.Email}
+                        pattern=".*@.*\..*" />
+                    </Col>
+                  </Row>
+                </Form.Group>
+
+                <Form.Group>
+                  <Row>
+                    <Col xs={12} md={4}>
+                      <Form.Label className="user-info__form-label">
+                        Date of birth
+                      </Form.Label>
+                    </Col>
+                    <Col>
+                      <Form.Control className="user-info__form-input" placeholder={this.props.userData.Birthday ? this.props.userData.Birthday.slice(0, 10) : 'yyyy-mm-dd'}
+                        pattern="^[0-9]{4}-[0-9]{2}-[0-9]{2}$" />
+                    </Col>
+                  </Row>
+                </Form.Group>
+
                 <Row>
                   <Col>
-                    <Form.Label>
-                      Username
-                    </Form.Label>
-                  </Col>
-                  <Col>
-                    <Form.Control placeholder={this.props.userData.Username}
-                                  pattern="^[a-zA-Z0-9]{5,}$"/>
+                    <Button className="btn btn-secondary btn-sm" variant="primary" type="submit" onClick={this.updateUserData} >Update</Button>
                   </Col>
                 </Row>
-              </Form.Group>
 
-              <Form.Group>
-                <Row>
-                  <Col>
-                    <Form.Label>
-                      Password
-                    </Form.Label>
-                  </Col>
-                  <Col>
-                    <Form.Control placeholder={'new password'}/>
-                  </Col>
-                </Row>
-              </Form.Group>
+              </Form>
+            </Col>
+            <Col>
+              <Button id="unregister-button" className="btn btn-secondary btn-sm" variant="danger" type="submit" onClick={this.unregister}>Unregister</Button>
+            </Col>
 
-              <Form.Group>
-                <Row>
-                  <Col>
-                    <Form.Label>
-                      E-mail
-                    </Form.Label>
-                  </Col>
-                  <Col>
-                    <Form.Control placeholder={this.props.userData.Email}
-                                  pattern=".*@.*\..*"/>
-                  </Col>
-                </Row>
-              </Form.Group>
-
-              <Form.Group>
-                <Row>
-                  <Col>
-                    <Form.Label>
-                      Date of birth
-                    </Form.Label>
-                  </Col>
-                  <Col>
-                    <Form.Control placeholder={this.props.userData.Birthday ? this.props.userData.Birthday.slice(0,10) : 'yyyy-mm-dd'}
-                                  pattern="^[0-9]{4}-[0-9]{2}-[0-9]{2}$"/>
-                  </Col>
-                </Row>
-              </Form.Group>
-
-              <Row>
-                <Col>
-                  <Button className="btn btn-secondary btn-sm" variant="primary" type="submit" onClick={this.updateUserData} >Update</Button>
-                </Col>
-              </Row>
-
-            </Form>
-          </Col>
-          <Col>
-            <Button className="btn btn-secondary btn-sm" variant="danger" type="submit" onClick={this.unregister}>Unregister</Button>
-          </Col>
-
-        </Row>
-
-        <p>My Favorite movies</p>
-        <Row>
-          { favoriteMovies.map(m => (
-              <Col md={3} key={m._id}>
+          </Row>
+        </div>
+        <div className="favorite-movies">
+          <h3>My Favorite movies</h3>
+          {userData.FavoriteMovies.length === 0 && <p id="no-favorites-message">You don't have any favorites yet. Go add some movies!</p>}
+          <Row className="favorite-movies__cards">
+            {favoriteMovies.map(m => (
+              <Col md={4} key={m._id}>
                 <Row>
                   <MovieCard movie={m} />
                 </Row>
                 <Row>
-                  <Button onClick={() => this.removeFromFavorites(m._id)}>remove favorite</Button>
+                  <Button type="button" className="btn btn-sm btn-warning" onClick={() => this.removeFromFavorites(m._id)}>remove favorite</Button>
                 </Row>
               </Col>
-          ))}
-        </Row>
+            ))}
+          </Row>
+        </div>
 
       </div>
     )
@@ -191,7 +195,7 @@ let mapStateToProps = state => {
   return { userData: state.userData, movies: state.movies }
 }
 
-export default connect(mapStateToProps, { setUserData } )(ProfileView);
+export default connect(mapStateToProps, { setUserData })(ProfileView);
 
 ProfileView.propTypes = {
   movies: PropTypes.arrayOf(
